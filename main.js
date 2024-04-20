431855d86bb3e5468957ccfa780b0aec00946b93
The Crystal King <dylanawhite11@gmail.com>
2024-04-20 06:09:16 -0500

    event listener expanded, added empty field validation

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-let inputBtn = document.getElementById('input-btn');
-let inputEl = document.getElementById('input-el');
-let myLeads = [];
+const inputBtn = document.getElementById('input-btn');
+const inputEl = document.getElementById('input-el');
+let myLeads = ['good lead', 'perfect job', 'juice cleanse'];
 
 inputBtn.addEventListener('click', () => {
-  console.log('button clicked');
+  if (inputEl.value !== '') {
+    myLeads.push(inputEl.value);
+    inputEl.value = '';
+
+    for (let i = 0; i < myLeads.length; i++) {
+      console.log(myLeads[i]);
+    }
+  }
 });
