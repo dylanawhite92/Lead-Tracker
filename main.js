1dd20e84012a3bc8c5b6fbfe65a354689abec4d1
The Crystal King <dylanawhite11@gmail.com>
2024-05-23 12:05:29 -0500

    starting functionality to save current tab

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let myLeads = [];
 const inputBtn = document.getElementById('input-btn');
 const deleteBtn = document.getElementById('delete-btn');
+const tabBtn = document.getElementById('tab-btn');
 const inputEl = document.getElementById('input-el');
 const listEl = document.getElementById('list-el');
 const leadsFromStorage = JSON.parse(localStorage.getItem('myLeads'));
@@ -8,6 +9,12 @@ const leadsFromStorage = JSON.parse(localStorage.getItem('myLeads'));
 if (leadsFromStorage) {
   renderLinkList(myLeads);
 }
+
+const tabs = [{ url: 'https://www.linkedin.com' }];
+
+tabBtn.addEventListener('click', () => {
+  console.log(tabs[0].url);
+});
 
 deleteBtn.addEventListener('dblclick', () => {
   localStorage.clear();
